8d314d8514d58c5dcc27c5c39b39710b3c156198
ninja <icymasa@gmail.com>
2017-11-12 20:29:04 +0900

    feat(admin): add auth

--- routes/admin.js
+++ routes/admin.js
@@ -12,23 +12,33 @@ const questionnaires = require("../data/questionnaires");
 
 const kotodamas_router = require('./kotodamas');
 const questions_router = require('./questions');
+const basicAuth = require('basic-auth-connect');
 
 admin_router.use('/kotodamas', kotodamas_router);
 admin_router.use('/questions', questions_router);
 
+admin_router.all('/', basicAuth(function(user, password) {
+  return user === 'admin' && password === 'mk1215skmt';
+}));
+
+
 //設問一覧ページ
 admin_router.get('/', function(req, res, next) {
+  co(function*(){
+    //設問ごとに表示
+    res.render("admin",{
+    });
+  }).catch(e=>next(e));
+});
+
+//設問一覧ページ
+admin_router.get('/users', function(req, res, next) {
   co(function*(){
     //ユーザー回答を網羅
     const all_user_answers = yield UserAnswer.findAndPopulateAnswers({}).exec();
 
-
-    //設問ごとに表示
-
-
-    res.render("admin",{
+    res.render("users",{
       all_user_answers,
-      questionnaires 
     });
   }).catch(e=>next(e));
 });
